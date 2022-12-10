b17478c20562ba8ebf03cf7fdea87c4ee998fd8f
ruchi06 <ruchi06@gmail.com>
2022-12-10 22:21:00 +0530

    Event

--- DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -2,8 +2,14 @@
 {
     internal class Program
     {
+        public static event DelEventHandler Del;
         static void Main(string[] args)
         {
+            Del += new DelEventHandler(EventProblem.India);
+            Del += new DelEventHandler(EventProblem.USA);
+            Del += new DelEventHandler(EventProblem.England);
+            Del.Invoke();
+
             ArrayDelegates.ImplementDelegates();
         }
     }
